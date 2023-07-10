ecd4568014d0af8198c2b873cb627d1275846333
archie <jfdoolster@gmail.com>
2023-07-10 12:04:06 -0600

    installer

--- install_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p $HOME/.config/Code\ -\ OSS/User/snippets
-ln -vf ./snippets/*    $HOME/.config/Code\ -\ OSS/User/snippets
-ln -vf ./settings.json $HOME/.config/Code\ -\ OSS/User/settings.json
